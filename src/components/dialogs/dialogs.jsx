7ced302de39e4464eef9289eec6f3e0200a0de3a
Untetzy <notescape262@gmail.com>
2021-03-22 12:05:58 +0200

    dev dialogs

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -7,12 +7,12 @@ const Dialogs = (props) => {
     <section className="dialogs">
       <div className="dialogs__inner">
         <div className="dialogs__users">
-          <DialogsUser name="Alexander Vetrov" message="Привет как дела" />
-          <DialogsUser name="Alexander Vetrov" message="Привет как дела" />
-          <DialogsUser name="Alexander Vetrov" message="Привет как дела" />
-          <DialogsUser name="Alexander Vetrov" message="Привет как дела" />
-          <DialogsUser name="Alexander Vetrov" message="Привет как дела" />
-          <DialogsUser name="Alexander Vetrov" message="Привет как дела" />
+          <DialogsUser id="/1" name="Alexander Vetrov" message="Привет как дела" />
+          <DialogsUser id="/2" name="Evgeniy Vetrov" message="Привет как дела" />
+          <DialogsUser id="/3" name="Sasha Vetrov" message="Привет как дела" />
+          <DialogsUser id="/4" name="Oleg Vetrov" message="Привет как дела" />
+          <DialogsUser id="/5" name="Grisha Vetrov" message="Привет как дела" />
+          <DialogsUser id="/6" name="Dima Vetrov" message="Привет как дела" />
         </div>
         <div className="dialogs__messages">
           <DialogsMessage />
